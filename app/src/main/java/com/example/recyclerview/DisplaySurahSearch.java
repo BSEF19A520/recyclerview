7d7d0961100b1d82493dc02cc22205b71b0e1ae1
Iffah Muzaffar <bsef19a520@pucit.edu.pk>
2022-09-12 17:35:49 +0500

    modified main and started writing display para and surah class

---
+++ app/src/main/java/com/example/recyclerview/DisplaySurahSearch.java
@@ -0,0 +1,14 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplaySurahSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_surah_search);
+    }
+}
